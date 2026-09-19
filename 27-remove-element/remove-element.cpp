3a3b28a81c8136f1faa74e495d36ac2acf3ca2da
DangerousPotato08 <288883122+DangerousPotato08@users.noreply.github.com>
2026-09-20 02:40:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (84.01%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int size = nums.size();
+        int k = size;
+        for(int i = 0 ; i < size ;i++){
+            if(nums[i]==val){
+                 for(int j = i;j<size-1;j++){
+                    nums[j] = nums[j+1];
+                }
+                size--;
+                k--;
+                i--;
+            }
+        }
+        return k;
+    }
+};
